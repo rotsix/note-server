451c43ece4fba0e1413717770202c347cb3de6ea
Victor Franzi <victor.franzi@gmail.com>
2019-11-04 18:59:47 +0100

    add alias to run server and dbcli on local

---
+++ alias.sh
@@ -0,0 +1,2 @@
+alias db_local='CONF_LOCATION=$(pwd)/configs/config_local.json go run $(pwd)/cmd/db/main.go'
+alias server_local='CONF_LOCATION=$(pwd)/configs/config_local.json go run $(pwd)/cmd/server/main.go'
